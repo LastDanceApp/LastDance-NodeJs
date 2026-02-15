89719a3fff0328476c5c5175e662b709b43bd789
junyong <rlawnsdyd530@gmail.com>
2026-02-16 04:47:35 +0900

    docs: openapi 추가

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { TransformInterceptor } from './common/interceptors/response.interceptor';
 import { ValidationPipe } from '@nestjs/common';
 import { GlobalExceptionFilter } from './common/filter/global-exception-filter';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import 'dotenv/config';
 
 async function bootstrap() {
@@ -16,6 +17,16 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Last Dance API')
+    .setDescription('Last Dance API description')
+    .setVersion('1.0')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
 
   await app.listen(process.env.PORT ?? 3000);
 }
